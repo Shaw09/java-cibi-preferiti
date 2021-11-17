1ceb16486cbf6318c6fb28900f0749890c311a8f
Shaw <michele_aliffi@yahoo.it>
2021-11-17 13:59:40 +0100

    End project, trying bonus.

--- src/org/generation/italy/array/CibiPreferiti.java
+++ src/org/generation/italy/array/CibiPreferiti.java
@@ -3,7 +3,14 @@ package org.generation.italy.array;
 public class CibiPreferiti {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String[] cibiPreferiti = {"Pizza", "Gelato", "Carbonara", "Lasagna", "Amatriciana", "Ragù", "Kebab", "Anelli di Cipolla", "Er Panino de Pippo a Tivoli", "Katsudon"};
+		
+		int lunghezzaArray = cibiPreferiti.length;
+		
+		System.out.println("La classifica è composta da: " + lunghezzaArray + " cibi");
+		
+		System.out.println("Il mio cibo più preferito tra i preferiti è " + cibiPreferiti[0]);
+		System.out.println("Il mio cibo meno preferito tra i preferiti è " + cibiPreferiti[9]);
 
 	}
 
